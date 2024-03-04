24672f967ca373a1f28d4cec6f089bd391e04e92
Robertson Hollingsworth <81538350+4RHollingsworth@users.noreply.github.com>
2024-03-04 13:19:12 -0500

    Added header (#18)
    * Added header
    
    Added the header, including a welcome message, the school logo, and "links" to various pages
    
    * Added search bar and played with spacing
    
    * Removed .DS_Store files

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  
+
+  <div class="header">
+    <div class="welcome-message">
+      <p>Welcome to TMU Connect! Your online marketplace for connecting with the TMU community.</p>
+    </div>
+    
+    <div class="logo-menu-container">
+      <img alt="TMU logo" src="@/assets/TMU_Logo.svg" width="100px">
+
+      <nav class="header-menu">
+        <ul>
+          <li><a href="#">ITEMS FOR SALE</a></li>
+          <li><a href="#">ITEMS WANTED</a></li>
+          <li><a href="#">ACADEMIC SERVICES</a></li>
+        </ul>
+      </nav>
+      
+      <div class="search-bar">
+        <input type="text" v-model="searchQuery" @input="performSearch" placeholder="Search TMU Connect...">
+        <button @click="performSearch">Search</button>
+      </div>
+
+      
+
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -41,18 +41,89 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  
+  .welcome-message
+  {
+    background-color:rgb(0, 0, 222);
+    padding: 8px;
+    text-align: center;
+    color: white;
+  }
+  
+  .welcome-message p
+  {
+    margin: 0;
+  }
+
+  .header
+  {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: white;
+    padding: 0px;
+    margin: 0px;
+    color: black;
+  }
+
+  .logo-menu-container
+  {
+    padding: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+  }
+  
+  .header-menu
+  {
+    display: flex;
+    align-items: center;
+  }
+
+  .header-menu ul
+  {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  .header-menu li
+  {
+    padding: 20px;
+    white-space: nowrap;
+  }
+
+  .header-menu a
+  {
+    text-decoration: none;
+    color: black;
+  }
+
+  .header-menu a:hover
+  {
+    border-bottom: 2px solid blue;
+  }
+
+  .search-bar
+  {
+    display: flex;
+    align-items: center;
+  }
+
+  .search-bar input
+  {
+    margin-right: 10px;
+  }
+
+  @media (max-width: 768px)
+  {
+    .search-bar
+    {
+      margin-left: auto;
+    }
+  }
+
 </style>
