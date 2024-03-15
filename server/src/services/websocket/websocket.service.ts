5dd184972c03e40e30bb632d838323d5ff0735fe
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-03-15 17:31:55 -0400

    feat: add websocket client cleanup on close

--- server/src/services/websocket/websocket.service.ts
+++ server/src/services/websocket/websocket.service.ts
@@ -6,6 +6,16 @@ import { WebSocket, WebSocketServer } from "ws";
 const wss = new WebSocketServer({ noServer: true });
 const clients = new Map<number, WebSocket[]>();
 
+// TODO: Add ping cleanup for dead clients
+
+wss.on("connection", (client: WebSocket, userId: number) => {
+  addClient(userId, client);
+
+  client.on("close", () => {
+    removeClient(client);
+  });
+});
+
 /**
  * Handle upgrade to websocket
  * @param request The incoming message
@@ -19,6 +29,30 @@ export function handleUpgrade(
   userId: number
 ) {
   wss.handleUpgrade(request, socket, head, (client: WebSocket) => {
-    clients.set(userId, [...(clients.get(userId) || []), client]);
+    wss.emit("connection", client, userId);
   });
+}
+
+/**
+ * Add a client to the clients map
+ * @param userId The user id
+ * @param client The client
+ */
+function addClient(userId: number, client: WebSocket) {
+  clients.set(userId, [...(clients.get(userId) || []), client]);
+}
+
+/**
+ * Remove a client from the clients map
+ * @param client The client
+ */
+function removeClient(client: WebSocket) {
+  for (const [userId, userClients] of clients) {
+    const index = userClients.indexOf(client);
+    if (index !== -1) {
+      userClients.splice(index, 1);
+      clients.set(userId, userClients);
+      return;
+    }
+  }
 }
