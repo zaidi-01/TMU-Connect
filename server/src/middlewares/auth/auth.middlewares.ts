3f1dafc4e9f2a2f98bad385dcecb3cb2fe746378
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-03-15 08:58:32 -0400

    feat: add access control middleware
    - Add access control middleware
    - Add user roles to the schema
    - Add user role to user model in the schema

--- server/src/middlewares/auth/auth.middlewares.ts
+++ server/src/middlewares/auth/auth.middlewares.ts
@@ -1,4 +1,13 @@
+import { Role, User } from "@prisma/client";
 import passport from "passport";
+
+/**
+ * Access levels for each role
+ */
+const accessLevels: Record<Role, number> = {
+  [Role.USER]: 0,
+  [Role.ADMIN]: 1,
+};
 
 /**
  * Middleware to authenticate requests
@@ -7,4 +16,19 @@ import passport from "passport";
  */
 export function authenticate(strategy: "jwt" | "local" = "jwt") {
   return passport.authenticate(strategy, { session: false });
+}
+
+/**
+ * Middleware to allow access to certain routes based on role
+ * @param role The role to allow access and above
+ */
+export function allow(role: Role) {
+  return (req: any, res: any, next: any) => {
+    if (accessLevels[(req.user as User).role] < accessLevels[role]) {
+      res.status(403).send("Forbidden");
+      return;
+    }
+
+    next();
+  };
 }
