d8c248de6193218bc73044c88d39dc92f80c5285
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-04-02 00:30:11 -0400

    feat: chat server

--- server/src/services/websocket/websocket.service.ts
+++ server/src/services/websocket/websocket.service.ts
@@ -21,9 +21,16 @@ const cleanupInterval = setInterval(() => {
   });
 }, 30 * 1000);
 
+
+/*Chat rooms */
+const roomClients = new Map<string, Set<ExtWebSocket>>();
+
 wss.on("connection", (client: ExtWebSocket, userId: number) => {
   client.isAlive = true;
   client.userId = userId;
+
+  joinRoom(client, 'general');
+
   client.sendMessage = (message, cb) =>
     client.send(JSON.stringify(message), cb);
   client.sendError = (message, error, cb) =>
@@ -62,4 +69,63 @@ export function handleUpgrade(
  */
 function heartbeat(client: ExtWebSocket) {
   client.isAlive = true;
+}
+
+/**
+ * Handles incoming messages
+ * @param client The WebSocket client
+ * @param message The incoming message
+ */
+function handleMessage(client: ExtWebSocket, message: string) {
+  const parsedMessage = JSON.parse(message);
+  const { action, data } = parsedMessage;
+
+  switch (action) {
+    case "joinRoom":
+      joinRoom(client, data.room);
+      break;
+    case "message":
+      sendMessageToRoom(client, data.room, data.message);
+      break;
+    default:
+      console.log("Unknown action:", action);
+  }
+}
+
+/**
+ * Joins a room
+ * @param client The WebSocket client
+ * @param room The name of the room to join
+ */
+function joinRoom(client: ExtWebSocket, room: string) {
+  // Create the room if it doesn't exist
+  if (!roomClients.has(room)) {
+    roomClients.set(room, new Set());
+  }
+  // Add the client to the room
+  const clients = roomClients.get(room);
+  if (clients) {
+    clients.forEach((recipient: ExtWebSocket) => {
+      recipient.sendMessage({ type: "message",action:"join", data: `${client.userId} has joined the room.` }, () => {});
+    });
+    clients.add(client);
+  } else {
+    console.log(`Room '${room}' does not exist.`);
+  }
+ 
+}
+
+/**
+ * Sends a message to all clients in a room
+ * @param client The WebSocket client
+ * @param room The name of the room
+ * @param message The message to send
+ */
+function sendMessageToRoom(client: ExtWebSocket, room: string, message: string) {
+  const clients = roomClients.get(room);
+  if (clients) {
+    clients.forEach((recipient: ExtWebSocket) => {
+      recipient.sendMessage({ type: "message", action: "send", data: message }, () => {});
+    });
+  }
 }
