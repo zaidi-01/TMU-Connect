9be9db74b44345edc6e87054c227ad92e6eb1a86
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-04-05 19:55:22 -0400

    feat: add ad search logic on home page

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,42 +1,40 @@
 <template>
-<div class="search-bar">
+  <div class="search-bar">
     <input
-        type="text"
-        v-model="searchQuery"
-        @input="performSearch"
-        placeholder="Search TMU Connect..."
+      type="text"
+      v-model="searchQuery"
+      @input="onSearch"
+      placeholder="Search TMU Connect..."
     />
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "SearchBar",
-    props: {
-        value: String,
+  name: "SearchBar",
+  props: {
+    value: String,
+  },
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$emit("query", this.searchQuery);
     },
-    data() {
-        return {
-            searchQuery: "",
-        };
-    },
-    methods: {
-        performSearch() {
-        },
-    },
+  },
 };
 </script>
 
 <style scoped>
-
 .search-bar input {
-    width: 350px;
-    height: 20px;
-    border-radius: 20px;
-    border: 2px solid rgb(0, 0, 222);
-    padding: 5px 10px;
-    font-size: 16px; 
+  width: 350px;
+  height: 20px;
+  border-radius: 20px;
+  border: 2px solid rgb(0, 0, 222);
+  padding: 5px 10px;
+  font-size: 16px;
 }
-
-
 </style>
